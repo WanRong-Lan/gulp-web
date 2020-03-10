8496e470864cc42484a1ba57c45c441002326886
LanWanRong <wanrong.lan@worldlinking.org>
2020-03-10 19:44:09 +0800

    # 增加config配置

--- .config.js
+++ .config.js
@@ -1,7 +1,8 @@
 const path = require('path')
 const DEV_PATH = path.resolve(__dirname, './src')// 开发目录
-const DIST_Path = path.resolve(__dirname, './dist') // 打包目录
+const DIST_Path = path.resolve(__dirname, './dist') // 打包目录，可以配置成app的目录
 
+// 代理配置，详细说明可以查看gulp-connect帮助文档
 const middlewares = [
   // proxy('/index', {
   //     target: 'http://youwebsite.com',
